ceb9bc4481d36d8e38d7a2ec28df294c9185a441
qifan-code <luckywillion@gmail.com>
2020-05-20 23:36:00 -0700

    add a toolbar goback button

--- app/src/main/java/com/example/chuanjiaoapp/test_activity.java
+++ app/src/main/java/com/example/chuanjiaoapp/test_activity.java
@@ -1,14 +1,27 @@
 package com.example.chuanjiaoapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class test_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //显示上方导航页面的 <-
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
+        /*
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+         */
     }
+
 
 }
