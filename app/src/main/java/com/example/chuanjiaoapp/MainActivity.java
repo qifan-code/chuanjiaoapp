762e98d477d2a0c45ea1690d4f7babd34e0314d6
qifan-code <luckywillion@gmail.com>
2020-05-21 01:24:22 -0700

    add forget password page

--- app/src/main/java/com/example/chuanjiaoapp/MainActivity.java
+++ app/src/main/java/com/example/chuanjiaoapp/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.chuanjiaoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button login_button_activity;
@@ -13,9 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //switch to test_activity
         login_button_activity = (Button)findViewById(R.id.login_button);
-
         login_button_activity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        //switch to forget_password_activity
+        TextView forget_password = (TextView)findViewById(R.id.forget_password);
+        forget_password.setText(getClickableSpan());
+        forget_password.setMovementMethod(LinkMovementMethod.getInstance());
     }
+
+    private SpannableString getClickableSpan(){
+        SpannableString spanStr = new SpannableString("忘记密码");
+        spanStr.setSpan(new UnderlineSpan(), 0, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spanStr.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                startActivity(new Intent(MainActivity.this, forget_password.class));
+            }
+        }, 0, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spanStr.setSpan(new ForegroundColorSpan(Color.rgb(103,135,193)), 0, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return spanStr;
+    }
+
 }
