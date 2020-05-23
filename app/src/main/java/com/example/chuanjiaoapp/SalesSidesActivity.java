9f3a8c5a8f940738599b95b13e0b06c0c4b0cb22
qifan-code <luckywillion@gmail.com>
2020-05-23 00:19:47 -0700

    add naive implementation for sales activity mainly deal with negavition sides on the left

---
+++ app/src/main/java/com/example/chuanjiaoapp/SalesSidesActivity.java
@@ -0,0 +1,14 @@
+package com.example.chuanjiaoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SalesSidesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sales_sides);
+    }
+}
--- app/src/main/java/com/example/chuanjiaoapp/test_activity.java
+++ app/src/main/java/com/example/chuanjiaoapp/test_activity.java
@@ -4,17 +4,28 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class test_activity extends AppCompatActivity {
-    @Override
+
+    Button sales_button;
     protected void onCreate(Bundle savedInstanceState) {
         //显示上方导航页面的 <-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
+        sales_button = (Button)findViewById(R.id.kefu);
+        sales_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(test_activity.this, SalesActivity.class);
+                startActivity(intent);
+            }
+        });
         /*
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
