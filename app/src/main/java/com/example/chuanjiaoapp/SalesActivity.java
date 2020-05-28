5f3b9f4c28ba8b4ba3dc99026e4783c252aefba2
qifan-code <luckywillion@gmail.com>
2020-05-28 15:23:45 -0700

    change sales activity list

--- app/src/main/java/com/example/chuanjiaoapp/SalesActivity.java
+++ app/src/main/java/com/example/chuanjiaoapp/SalesActivity.java
@@ -3,6 +3,9 @@ package com.example.chuanjiaoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import java.lang.reflect.Method;
 
 public class SalesActivity extends AppCompatActivity {
 
@@ -11,4 +14,21 @@ public class SalesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sales);
     }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (menu != null) {
+            if (menu.getClass().getSimpleName().equalsIgnoreCase("MenuBuilder")) {
+                try {
+                    Method method = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    method.setAccessible(true);
+                    method.invoke(menu, true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
+
 }
--- app/src/main/java/com/example/chuanjiaoapp/test_activity.java
+++ app/src/main/java/com/example/chuanjiaoapp/test_activity.java
@@ -10,10 +10,15 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.button.MaterialButton;
+
 public class test_activity extends AppCompatActivity {
 
     Button sales_button;
     Button add_new_project_button;
+    Button check_project_detail;
+    Button new_project_material_apply;
+    Button material_return;
     protected void onCreate(Bundle savedInstanceState) {
         //显示上方导航页面的 <-
 
@@ -36,6 +41,31 @@ public class test_activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        check_project_detail = (Button) findViewById(R.id.gongdanxiangqing);
+        check_project_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(test_activity.this, ProjectDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+        new_project_material_apply = (Button) findViewById(R.id.new_project_material_apply);
+        new_project_material_apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(test_activity.this, NewProjectMaterialApplyActivity.class);
+                startActivity(intent);
+            }
+        });
+        material_return = (Button) findViewById(R.id.material_return);
+        material_return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (test_activity.this, MaterialReturnActivity.class);
+                startActivity(intent);
+            }
+        });
+
         /*
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
